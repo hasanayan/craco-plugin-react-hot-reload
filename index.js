41d8e30a1b76516b73795e7ee5d93175a29e342f
Matus <hruzmatus@gmail.com>
2019-03-09 19:44:55 +0100

    feat: add react-hot-dom alias

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
 module.exports = {
-    overrideWebpackConfig: ({
-        webpackConfig,
-        cracoConfig,
-        pluginOptions,
-        context: {
-            env,
-            paths
-        }
-    }) => {
-        if (pluginOptions && pluginOptions.preText) {
-            console.log(pluginOptions.preText);
-        }
-
-        if (env === "production")
-            return webpackConfig;
-
-        const condition = u => typeof u === 'object' && u.loader && u.loader.includes('eslint-loader');
-        const rule = webpackConfig.module.rules.find(rule => rule.use && rule.use.some(condition));
-
-        if (rule) {
-            const use = rule.use.find(condition);
-            if (use) {
-                use.options.emitWarning = true;
-            }
-        }
-        return webpackConfig;
-    },
-
-    overrideCracoConfig: ({
-        cracoConfig,
-        pluginOptions,
-        context: {
-            env,
-            paths
-        }
-    }) => {
-
-        if (!cracoConfig.babel)
-            cracoConfig.babel = {};
-
-        if (!cracoConfig.babel.plugins)
-            cracoConfig.babel.plugins = [];
-
-        cracoConfig.babel.plugins.push("react-hot-loader/babel")
-        return cracoConfig;
+  overrideWebpackConfig: ({
+    webpackConfig,
+    pluginOptions,
+    context: {
+      env
     }
+  }) => {
+    if (env === "production") {
+      return webpackConfig;
+    }
+
+    const conf = { ...webpackConfig };
+
+    if (!conf || !conf.module || !conf.module.rules) {
+      return webpackConfig;
+    }
+
+    const condition = u => typeof u === 'object' && u.loader && u.loader.includes('eslint-loader');
+    const rule = conf.module.rules.find(rule => rule.use && rule.use.some(condition));
+
+    if (rule) {
+      const use = rule.use.find(condition);
+      if (use) {
+        use.options.emitWarning = true;
+      }
+    }
+
+    return {
+      ...conf,
+      resolve: {
+        ...(conf.resolve || {}),
+        alias: {
+          ...((conf.resolve && conf.resolve.alias) || {}),
+          'react-dom': '@hot-loader/react-dom'
+        }
+      }
+    };
+  },
+
+  overrideCracoConfig: ({
+    cracoConfig
+  }) => {
+    return {
+      ...cracoConfig,
+      babel: {
+        ...(cracoConfig.babel || {}),
+        plugins: [
+          ...(cracoConfig.babel.plugins || []),
+          "react-hot-loader/babel"
+        ]
+      }
+    };
+  }
 };
